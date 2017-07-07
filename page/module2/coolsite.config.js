7ce3b963494d089689f7cad66365fce189fee278
linzuhan <zuhanlin@pptv.com>
2017-07-07 18:24:43 +0800

    init commit

---
+++ page/module2/coolsite.config.js
@@ -0,0 +1,51 @@
+
+var actions = [
+  {
+    "element_id": "wx_button86aaad14", 
+    "data": {
+      "args": {
+        "e_ids": [
+          "wx_view_74a0d568"
+        ], 
+        "cla": "c-state1", 
+        "st": 1, 
+        "a_ids": []
+      }, 
+      "type": 0, 
+      "exec": 26
+    }, 
+    "id": "M_c9c3eaf9a5124a1b"
+  }
+];
+
+var animations = [];
+
+var timelines = [
+  {
+    "iType": 0, 
+    "isNew": true, 
+    "animations": [], 
+    "element_id": "body_2222f102a98b11f1", 
+    "data": {
+      "type": 0, 
+      "t": {
+        "rv": 0, 
+        "rp": 0, 
+        "wa": 0, 
+        "de": 0, 
+        "st": 1, 
+        "du": 1, 
+        "es": 0
+      }, 
+      "d": {}
+    }, 
+    "id": "M_5a6a44dced1075f6"
+  }
+];
+
+getApp().coolsite360.DATA[__wxRoute] = {
+   animations:animations,
+   actions:actions,
+   timelines:timelines
+};
+
